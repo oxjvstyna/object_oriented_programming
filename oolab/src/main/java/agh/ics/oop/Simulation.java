d0a8dc917b8a69afda8806237e729e9528aaeb16
Igoru <igorpie1705@gmail.com>
2024-11-13 02:56:42 +0100

    Ulepszono simulation, teraz jest idealnie.

--- oolab/src/main/java/agh/ics/oop/Simulation.java
+++ oolab/src/main/java/agh/ics/oop/Simulation.java
@@ -3,30 +3,30 @@ package agh.ics.oop;
 import agh.ics.oop.model.*;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Simulation {
-    private final Map<Vector2d, Animal> animals;
-    private final List<MoveDirection> moves = new ArrayList<>();
+    private final List<Animal> animals;
+    private List<MoveDirection> moves = new ArrayList<>();
     private final RectangularMap currentMap;
     public Simulation(List<Vector2d> positions, List<MoveDirection> moves, RectangularMap currentMap) {
-        this.moves.addAll(moves);
+        this.animals = new ArrayList<>();
         this.currentMap = currentMap;
-        animals = currentMap.getAnimals();
+        this.moves = moves;
+
         for(Vector2d position : positions){
             Animal newAnimal = new Animal(position);
-            currentMap.place(newAnimal);
+            if(currentMap.place(newAnimal)){
+                animals.add(newAnimal);
+            }
         }
     }
     public void run(){
-        List<Animal> animalList = new ArrayList<>(animals.values());
-        int animalCount = animalList.size();
+        int animalCount = animals.size();
         int moveCount = moves.size();
         System.out.println(currentMap);
         for (int i = 0; i < moveCount; i++) {
-            currentMap.move(animalList.get(i % animalCount), moves.get(i));
+            currentMap.move(animals.get(i % animalCount), moves.get(i));
             System.out.println(currentMap);
         }
     }
--- oolab/src/main/java/agh/ics/oop/model/RectangularMap.java
+++ oolab/src/main/java/agh/ics/oop/model/RectangularMap.java
@@ -9,7 +9,6 @@ public class RectangularMap implements WorldMap {
     private final int width;
     private final int height;
 
-
     public RectangularMap(int width, int height) {
         this.width = width;
         this.height = height;
